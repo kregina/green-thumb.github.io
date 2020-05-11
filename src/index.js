21f09b26d07698d0eef9fd784c56618b3c8f0134
Kelly Silva <kelly@kellyregina.com.br>
2020-05-10 21:07:35 -0300

    implements search section

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
 import './index.css';
 import imgLogo from './images/logo.png';
+import imgSun from './images/sun.png';
+import imgDog from './images/dog.png';
+import imgWateringcan from './images/wateringcan.png';
 
-const logo = document.getElementById('logo');
-logo.src = imgLogo;
+const logo = document.getElementById('logo').src = imgLogo;
+const sun = document.getElementById('sunlight').src = imgSun;
+const water = document.getElementById('chew').src = imgDog;
+const dog = document.getElementById('water').src = imgWateringcan;
+
+const sunList = ['high', 'low', 'no'];
+const waterList = ['false', 'true'];
+const dogList = ['daily', 'regularly', 'rarely'];
+
+const selectSun = document.getElementById('selectSun');
+createSelectOption(selectSun, sunList);
+
+const selectWater = document.getElementById('selectWater');
+createSelectOption(selectWater, waterList);
+
+const selectChew = document.getElementById('selectChew');
+createSelectOption(selectChew, dogList);
+
+function createSelectOption(select, arr) {
+  arr.forEach((item, i) => {
+    let option = document.createElement('option');
+    option.value = i;
+    option.text = item;
+
+    select.appendChild(option);
+  });
+}
